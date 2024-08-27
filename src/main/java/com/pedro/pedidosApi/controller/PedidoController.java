22a7af725285d7cd28a810d8411cf4cb90d321e2
FreitasPedro <pedrospf2018@gmail.com>
2024-08-27 18:02:29 -0300

    feat: Documentando API com Swagger
    - Primeiros passos e configuracoes

--- src/main/java/com/pedro/pedidosApi/controller/PedidoController.java
+++ src/main/java/com/pedro/pedidosApi/controller/PedidoController.java
@@ -1,6 +1,11 @@
 package com.pedro.pedidosApi.controller;
 
 import com.pedro.pedidosApi.entity.Pedido;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,11 +15,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
+@Tag(name = "Pedidos", description = "Recurso para gerenciar pedidos")
 @Slf4j
 @RestController
 @RequestMapping("/api/v1/pedidos")
 public class PedidoController {
 
+    @Operation(
+            summary = "Cria um novo pedido", description = "Contém operações para criar um novo pedido",
+            responses = @ApiResponse(responseCode = "201", description = "Pedido criado com sucesso",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = Pedido.class)))
+            )
     @PostMapping
     public ResponseEntity<Pedido> criarPedido(@RequestBody Pedido pedido) {
          log.info("Criando pedido: {}", pedido.toString());
