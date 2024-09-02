3756a31c0f76569446c00ad026e74065888664e7
FreitasPedro <pedrospf2018@gmail.com>
2024-09-02 16:37:03 -0300

    :wrench: chore: Upload 'Processador' initial Files
    - Importando novo projeto do Spring Initializr
    - Adicionando depedencias necessárias

---
+++ processador/src/main/java/com/pedro/pedidos/processador/entity/Pedido.java
@@ -0,0 +1,29 @@
+package com.pedro.pedidos.processador.entity;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import com.pedro.pedidos.processador.entity.enums.Status;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Data
+public class Pedido {
+
+    private UUID id = UUID.randomUUID();
+    private String cliente;
+
+    private Integer valorTotal;
+    private String emailNotificacao;
+
+    private List<ItemPedido> itens = new ArrayList<>();
+
+    private Status status;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime dataHora = LocalDateTime.now();
+
+}
---
+++ processador/src/main/java/com/pedro/pedidos/processador/entity/enums/Status.java
@@ -0,0 +1,5 @@
+package com.pedro.pedidos.processador.entity.enums;
+
+public enum Status {
+    EM_PROCESSAMENTO, PROCESSADO;
+}
